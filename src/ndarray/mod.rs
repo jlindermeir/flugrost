29318ff5fdb24e3d258f7f2a4ada8c6652a60bc1
jlindermeir <jakob@skasa.de>
2024-01-12 20:46:20 +0100

    Implement Shape

--- src/ndarray/mod.rs
+++ src/ndarray/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 struct Const<const N: usize>;
 
 trait Dim {
@@ -10,7 +12,28 @@ impl<const N: usize> Dim for Const<N> {
     }
 }
 
-trait Shape {
+pub(crate) trait Shape {
     const N_DIMS: usize;
     fn shape(&self) -> [usize; Self::N_DIMS];
+}
+
+impl Shape for () {
+    const N_DIMS: usize = 0;
+    fn shape(&self) -> [usize; Self::N_DIMS] {
+        []
+    }
+}
+
+impl<D1: Dim> Shape for (D1, ) {
+    const N_DIMS: usize = 1;
+    fn shape(&self) -> [usize; Self::N_DIMS] {
+        [self.0.size()]
+    }
+}
+
+impl<D1: Dim, D2: Dim> Shape for (D1, D2) {
+    const N_DIMS: usize = 2;
+    fn shape(&self) -> [usize; Self::N_DIMS] {
+        [self.1.size(), self.0.size()]
+    }
 }
