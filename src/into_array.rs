66ad93f7b31591da4a96ec4473520753718afa20
jlindermeir <jakob@skasa.de>
2024-12-30 21:10:13 +0100

    Rename a function

--- src/into_array.rs
+++ src/into_array.rs
@@ -3,11 +3,11 @@ use crate::ndarray::NDArray;
 use crate::shape::{Rank0, Shape};
 
 trait IntoNDArray<S: Shape, T: DType> {
-    fn into_ndarray(self) -> NDArray<S, T>;
+    fn into_array(self) -> NDArray<S, T>;
 }
 
 impl<T: DType> IntoNDArray<Rank0, T> for T {
-    fn into_ndarray(self) -> NDArray<Rank0, T> {
+    fn into_array(self) -> NDArray<Rank0, T> {
         NDArray::new(vec![self])
     }
 }
@@ -17,8 +17,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_into_ndarray() {
-        let array = 42.into_ndarray();
+    fn test_into_array() {
+        let array = 42.into_array();
         assert_eq!(array.get(&[]), Ok(42));
     }
 }
